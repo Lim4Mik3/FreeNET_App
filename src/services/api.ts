6b6e004c4fcf6d3edce5d5148b33551633a1e3fd
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-10-28 17:56:19 -0300

    refactor: add a https address viacep

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://viacep.com.br/ws',
+  baseURL: 'https://viacep.com.br/ws',
   headers: {
     'Content-Type': 'application/json',
   },
