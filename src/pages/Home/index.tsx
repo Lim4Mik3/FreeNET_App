688e50ffed11643ecda3a832f20042cd2850a7fc
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-10-28 00:07:35 -0300

    feat: create input form at home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,39 @@
 import { ReactElement } from 'react';
 
-import { Header, Container, Actions } from './styles';
+import { Header, Container, Actions, Content } from './styles';
 
 export function Home(): ReactElement {
   return (
-    <Header>
-      <Container>
-        <strong>
-          Free<span>NET</span>
-        </strong>
+    <>
+      <Header>
+        <Container>
+          <strong>
+            Free<span>NET</span>
+          </strong>
 
-        <Actions>
-          <h3>11 3601-3601</h3>
-          <button type="button">CONTRATE</button>
-        </Actions>
-      </Container>
-    </Header>
+          <Actions>
+            <h3>11 3601-3601</h3>
+            <button type="button">CONTRATE</button>
+          </Actions>
+        </Container>
+      </Header>
+
+      <Content>
+        <div>
+          <h1>Olá visitante, seja bem vindo!</h1>
+
+          <span>
+            Para buscarmos os planos disponíveis, por favor insira seu CEP
+            abaixo.
+          </span>
+          <form>
+            <p>Apenas números</p>
+            <input id="zipcode" type="text" autoComplete="off" />
+
+            <button type="submit">Buscar</button>
+          </form>
+        </div>
+      </Content>
+    </>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -62,3 +62,71 @@ export const Actions = styled.div`
     }
   }
 `;
+
+export const Content = styled.main`
+  width: 100%;
+  height: 100%;
+
+  div {
+    width: 100%;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 6rem 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+      font-size: 4rem;
+      color: var(--red-chill);
+    }
+
+    span {
+      margin-top: 4rem;
+      font-size: 2rem;
+      font-weight: thin;
+      color: var(--title);
+    }
+
+    form {
+      padding: 5rem 3rem;
+
+      p {
+        font-size: 0.9rem;
+      }
+
+      input {
+        width: 20rem;
+        background-color: var(--white);
+        height: 4rem;
+        border-radius: 20px;
+        border: 1px solid #333;
+        padding: 0 1rem;
+        font-size: 2rem;
+        color: var(--black);
+      }
+
+      button {
+        display: block;
+        margin-top: 1rem;
+        width: 20rem;
+        height: 4rem;
+        border-radius: 20px;
+        border: 1px solid red;
+        background-color: #e63f55;
+
+        font-size: 1.4rem;
+        color: var(--white);
+        text-transform: uppercase;
+        letter-spacing: 1px;
+
+        transition: filter 0.2s;
+
+        &:hover {
+          filter: brightness(0.8);
+        }
+      }
+    }
+  }
+`;
