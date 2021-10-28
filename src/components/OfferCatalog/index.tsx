759b2b6a1a05774198e28bfa2cc85fbcc28bd5ec
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-10-28 15:42:55 -0300

    feat: formatingn the price offer value

--- src/components/OfferCatalog/index.tsx
+++ src/components/OfferCatalog/index.tsx
@@ -1,25 +1,41 @@
+import { useEffect } from 'react';
+import { useOffers } from '../../hooks/useOffers';
 import { OfferCard } from './OfferCard';
 
 import { Container } from './styles';
 
 const OfferCatalog: React.FC = () => {
+  const { loadOffers, userData } = useOffers();
+
+  useEffect(() => {
+    loadOffers();
+  }, []);
+
+  let offers;
+
+  if (userData.offers) {
+    offers = userData.offers.map((offer) => ({
+      ...offer,
+      formatedPrice: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(offer.price / 100),
+    }));
+  }
+
   return (
     <Container>
-      <OfferCard
-        title="Oferta 01"
-        description="Tenha 80 megas de internet para usar o mes todo!"
-        price="R$ 50,99"
-      />
-      <OfferCard
-        title="Oferta 01"
-        description="Tenha 80 megas de internet para usar o mes todo!"
-        price="R$ 50,99"
-      />
-      <OfferCard
-        title="Oferta 01"
-        description="Tenha 80 megas de internet para usar o mes todo!"
-        price="R$ 50,99"
-      />
+      {offers &&
+        offers.map((offer) => (
+          <OfferCard
+            key={offer.id}
+            title={offer.name}
+            description={`Tenha ${
+              offer.id * 100
+            } megas de internet para usar o mes todo!`}
+            price={offer.formatedPrice}
+          />
+        ))}
     </Container>
   );
 };
